a67664cd4b842ef132a801d9df1427ed6b1e5e39
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2021-12-25 19:13:22 +0530

    Fixed some erros

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
-export function HelloWorld() {
-	console.log("Hello World");
+import { useEffect, useState } from "react";
+import { BehaviorSubject } from "rxjs";
+
+export function makeStore<T>(intialValue: T): [BehaviorSubject<T>, () => [T]] {
+	const store = new BehaviorSubject(intialValue);
+	const hook = (): [T] => {
+		const [state, setState] = useState(store.value);
+		useEffect(() => {
+			const subscription = store.subscribe((state: T) => {
+				setState(state);
+			});
+			return () => {
+				subscription.unsubscribe();
+			};
+		});
+		return [state];
+	};
+	return [store, hook];
 }
