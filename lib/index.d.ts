5b9f9d24b589a03a12222c4580a9b246ae702cb7
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2022-04-19 14:19:36 +0530

    Made it in single file

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,2 +1,16 @@
-import { Store, StoreCallbacks } from "./store";
+import { Subscription } from "rxjs";
+export declare type StoreCallbacks<T> = {
+    beforeUpdate?: (state: T) => void;
+    afterUpdate?: (state: T) => void;
+};
+export declare class Store<T> {
+    private _store;
+    private _callbacks;
+    constructor(initialValue: T, callbacks: StoreCallbacks<T>);
+    currentValue(): T;
+    set(newValue: T): void;
+    merge(newValue: Partial<T>): void;
+    subscribe(callback: (state: T) => void): Subscription;
+    unsubscribe(): void;
+}
 export declare function makeStore<T>(intialValue: T, callbacks?: StoreCallbacks<T>): [Store<T>, () => [T]];
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,50 @@
 import { useEffect, useState } from "react";
-import { Store, StoreCallbacks } from "./store";
+import { BehaviorSubject, Subscription } from "rxjs";
+
+export type StoreCallbacks<T> = {
+	beforeUpdate?: (state: T) => void;
+	afterUpdate?: (state: T) => void;
+};
+
+export class Store<T> {
+	private _store: BehaviorSubject<T>;
+	private _callbacks: StoreCallbacks<T> = {};
+
+	constructor(initialValue: T, callbacks: StoreCallbacks<T>) {
+		this._store = new BehaviorSubject<T>(initialValue);
+		this._store.subscribe((value: T) => {});
+		this._callbacks = callbacks;
+	}
+
+	currentValue(): T {
+		return this._store.value;
+	}
+
+	set(newValue: T): void {
+		// Before update
+		if (this._callbacks.beforeUpdate) {
+			this._callbacks.beforeUpdate(this._store.value);
+		}
+		// Update value
+		this._store.next({ ...newValue });
+		// After update
+		if (this._callbacks.afterUpdate) {
+			this._callbacks.afterUpdate(this._store.value);
+		}
+	}
+
+	merge(newValue: Partial<T>): void {
+		this.set({ ...this._store.value, ...newValue });
+	}
+
+	subscribe(callback: (state: T) => void): Subscription {
+		return this._store.subscribe(callback);
+	}
+
+	unsubscribe() {
+		this._store.unsubscribe();
+	}
+}
 
 export function makeStore<T>(
 	intialValue: T,
